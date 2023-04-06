c8bf7095ad82188896c5d5a123d435a3186f1b25
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-04-06 19:56:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-//code here 
+//code 
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers/index.js";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
